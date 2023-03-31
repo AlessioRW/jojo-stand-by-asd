393b9e8ce4bbc82658cefd9a47173eae5e00c702
Alessio <alessiowright@outlook.com>
2023-03-31 23:35:22 +0100

    working, without styling

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,18 +1,37 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { calculateStand } from "../utils/calculateStand"
 import { useLocation } from "react-router-dom"
 
 export function Results(){
-
-    const score = useLocation().state.score
-
+    const [standsByScore, setStandsByScore] = useState([])
+    const location =  useLocation() 
+    const score = location.state.score
+    
     useEffect(() => {
-        calculateStand(score)
-    })
+        setStandsByScore(calculateStand(score))
+    }, [])
 
-    return (
-        <div className="results page">
-            <h1>Results</h1>
-        </div>
-    )
+    if (standsByScore.length > 0){
+        return (
+            <div className="results page">
+                <h1>Results</h1>
+    
+                <div className="most-similar">
+                    <h2>Your most similar stand is: {standsByScore[0][0]}</h2>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="results page">
+                <h1>Results</h1>
+    
+                <div className="loading">
+                    <h2>Your results are being loaded</h2>
+                </div>
+            </div>
+        )
+    }
+    
+    
 }
--- src/utils/calculateStand.js
+++ src/utils/calculateStand.js
@@ -2,7 +2,6 @@ const standStats = require('../stand-stats.json')
 
 export function calculateStand(scores){
     const score = scores.split('-')
-    //console.log(score)
     //define spectrum stats to make it easier for me
     
     const dep = parseInt(score[0])
@@ -24,19 +23,32 @@ export function calculateStand(scores){
     const precision = (post+eye)/2
     const potential = (tics+agg)/2
 
-    console.log(power,speed,range,stamina,precision,potential)
     
     const standsByDiff = []
     for (let stand of standStats){
         let totalDiff = 0
-        totalDiff += Math.abs(stand.stats.power, power)
-        totalDiff += Math.abs(stand.stats.speed, speed)
-        totalDiff += Math.abs(stand.stats.range, range)
-        totalDiff += Math.abs(stand.stats.stamina, stamina)
-        totalDiff += Math.abs(stand.stats.precision, precision)
-        totalDiff += Math.abs(stand.stats.potential, potential)
+        totalDiff += Math.abs(stand.stats.power - power)
+        totalDiff += Math.abs(stand.stats.speed - speed)
+        totalDiff += Math.abs(stand.stats.range - range)
+        totalDiff += Math.abs(stand.stats.stamina - stamina)
+        totalDiff += Math.abs(stand.stats.precision - precision)
+        totalDiff += Math.abs(stand.stats.potential - potential)
 
-        console.log(stand.name,totalDiff)
-
+        let addedStand = false
+        if (standsByDiff.length <= 0){
+            standsByDiff.push([stand.name, totalDiff])
+        } else {
+            for (let [i,checkStand] of standsByDiff.entries()){
+                if (checkStand[1] < totalDiff){
+                    standsByDiff.splice(i,0, [stand.name, totalDiff])
+                    addedStand = true
+                    break
+                }
+            }
+        }
+        if (!addedStand){
+            standsByDiff.push([stand.name, totalDiff])
+        }
     }
+    return standsByDiff.reverse()
 }
